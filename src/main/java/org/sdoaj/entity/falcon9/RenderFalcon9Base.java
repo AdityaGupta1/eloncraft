3507a6e84d2fafc96370e302edd9ca211856feb7
AdityaGupta1 <adityag@gmail.com>
2019-04-03 01:57:31 -0700

    stuff

--- src/main/java/org/sdoaj/entity/falcon9/RenderFalcon9Base.java
+++ src/main/java/org/sdoaj/entity/falcon9/RenderFalcon9Base.java
@@ -6,10 +6,10 @@ import net.minecraft.util.ResourceLocation;
 import org.sdoaj.eloncraft.Main;
 
 public class RenderFalcon9Base extends RenderLiving<EntityFalcon9Base> {
-    private ResourceLocation texture = new ResourceLocation(Main.MODID, "textures/entities/falcon9_base.png");
+    private ResourceLocation texture = new ResourceLocation(Main.MODID, "textures/entities/falcon9/falcon9_base.png");
 
     public RenderFalcon9Base(RenderManager manager) {
-        super(manager, new ModelFalcon9Base(), 1.0f);
+        super(manager, new ModelFalcon9Base(), 0.4f * ModelFalcon9Base.modelScale);
     }
 
     @Override
