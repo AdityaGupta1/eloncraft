099b7ec605dcff58aafa5249b8fb7d6d65768e51
AdityaGupta1 <adityag@gmail.com>
2019-04-13 00:51:33 -0700

    fixed #49 + a lot of other launchpad things

--- src/main/java/org/sdoaj/item/blocks/launch/BlockLaunchpad.java
+++ src/main/java/org/sdoaj/item/blocks/launch/BlockLaunchpad.java
@@ -3,6 +3,9 @@ package org.sdoaj.item.blocks.launch;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.gui.chat.IChatListener;
+import net.minecraft.command.CommandSenderWrapper;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
@@ -14,69 +17,76 @@ import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import org.sdoaj.entity.falcon9.EntityFalcon9Base;
-import org.sdoaj.entity.falcon9.IRocketController;
 import org.sdoaj.item.blocks.BlockNotFull;
 import org.sdoaj.item.blocks.ModBlocks;
 import org.sdoaj.item.items.ModItems;
 
-public class BlockLaunchpad extends BlockNotFull implements IRocketController {
+import java.util.HashMap;
+import java.util.Optional;
+
+public class BlockLaunchpad extends BlockNotFull {
+    private static final int r = 3;
+
     public BlockLaunchpad(String name, Material material) {
         super(name, material, new AxisAlignedBB(0.0, 0.0, 0.0, 1.0, 0.25, 1.0));
     }
 
-    private EntityFalcon9Base rocket = null;
-    private Vec3d rocketPos = null;
+    private static HashMap<BlockPos, EntityFalcon9Base> rockets = new HashMap<>();
 
-    @Override
-    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        if (rocket != null) {
-            return false;
+    private Optional<BlockPos> getCenterLaunchpad(BlockPos pos) {
+        for (BlockPos otherPos : rockets.keySet()) {
+            if (Math.abs(pos.getX() - otherPos.getX()) <= 3 && Math.abs(pos.getZ() - otherPos.getZ()) <= 3) {
+                return Optional.of(otherPos);
+            }
         }
 
-        if (player.getHeldItem(hand).getItem() != ModItems.FALCON9_BASE) {
-            return false;
+        return Optional.empty();
+    }
+
+    private void sendErrorMessage(Entity receiver, String message) {
+        receiver.sendMessage(new TextComponentString(TextFormatting.RED + message));
+    }
+
+    private int isValid(World world, BlockPos pos) {
+        if (world.getBlockState(pos) != this.getDefaultState()) {
+            return -1;
         }
 
         int x = pos.getX();
         int y = pos.getY();
         int z = pos.getZ();
 
-        if (!world.isRemote) {
-            return false;
+        if (!checkBlocks(world, new BlockPos(x - r, y, z - r), new BlockPos(x + r, y, z + r), ModBlocks.LAUNCHPAD)) {
+            return 1;
         }
 
-        if (!checkBlocks(world, new BlockPos(x - 3, y - 1, z - 3), new BlockPos(x + 3, y - 1, z + 3), Blocks.OBSIDIAN)) {
-            player.sendMessage(new TextComponentString(TextFormatting.RED + "Missing obsidian!"));
-            return false;
+        if (!checkBlocks(world, new BlockPos(x - r, y + 1, z - r), new BlockPos(x + r, y + 1, z + r), Blocks.AIR)) {
+            return 2;
         }
 
-        if (!checkBlocks(world, new BlockPos(x - 3, y, z - 3), new BlockPos(x + 3, y, z + 3), ModBlocks.LAUNCHPAD)) {
-            player.sendMessage(new TextComponentString(TextFormatting.RED + "Missing launchpad!"));
-            return false;
+        for (int x1 = x - r; x1 <= x + r; x1++) {
+            for (int z1 = z - r; z1 <= z + r; z1++) {
+                // if a launchpad that this one would use as part of its r*r square is already part of a square, return 3
+                if (getCenterLaunchpad(new BlockPos(x1, y, z1)).isPresent()) {
+                    return 3;
+                }
+            }
         }
 
-        if (!checkBlocks(world, new BlockPos(x - 3, y + 1, z - 3), new BlockPos(x + 3, y + 1, z + 3), Blocks.AIR)) {
-            player.sendMessage(new TextComponentString(TextFormatting.RED + "Space above launchpad is obstructed!"));
-            return false;
-        }
-
-        if (!player.isCreative()) {
-            player.getHeldItem(hand).shrink(1);
-        }
-
-        rocketPos = new Vec3d(pos).addVector(0.5, 0.25, 0.5);
-        rocket = new EntityFalcon9Base(world, this);
-        world.spawnEntity(this.rocket);
-
-        return true;
+        return 0;
     }
 
-    @Override
-    public void breakBlock(World world, BlockPos pos, IBlockState state) {
-        if (rocket != null) {
-            rocket.setDead();
-            rocket = null;
+    private Optional<BlockPos> findNearestValidLaunchpad(World world, BlockPos pos) {
+        for (int x = pos.getX() - r; x <= pos.getX() + r; x++) {
+            for (int z = pos.getZ() - r; z <= pos.getZ() + r; z++) {
+                BlockPos otherPos = new BlockPos(x, pos.getY(), z);
+                if (isValid(world, otherPos) == 0) {
+                    return Optional.of(otherPos);
+                }
+            }
         }
+
+        return Optional.empty();
     }
 
     private boolean checkBlocks(World world, BlockPos a, BlockPos b, Block block) {
@@ -94,7 +104,56 @@ public class BlockLaunchpad extends BlockNotFull implements IRocketController {
     }
 
     @Override
-    public Vec3d getPos() {
-        return rocketPos;
+    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        Optional<BlockPos> center = getCenterLaunchpad(pos);
+        // if this launchpad is already part of another launchpad's r*r square, activate that launchpad instead
+        if (center.isPresent() && !center.get().equals(pos)) {
+            return onBlockActivated(world, center.get(), state, player, hand, facing, hitX, hitY, hitZ);
+        }
+
+        if (!world.isRemote || rockets.containsKey(pos) || player.getHeldItem(hand).getItem() != ModItems.FALCON9_BASE) {
+            return false;
+        }
+
+        switch (isValid(world, pos)) {
+            case 1:
+                Optional<BlockPos> nearest = findNearestValidLaunchpad(world, pos);
+                if (nearest.isPresent()) {
+                    return onBlockActivated(world, nearest.get(), state, player, hand, facing, hitX, hitY, hitZ);
+                }
+
+                final int d = r * 2 + 1;
+                sendErrorMessage(player, "Missing launchpad(s)! Required platform size: " + d + "x" + d + "!");
+                return false;
+            case 2:
+                sendErrorMessage(player, "Space above launchpad is obstructed!");
+                return false;
+            case 3:
+                sendErrorMessage(player, "Nearby launchpads already have a rocket!");
+                return false;
+            default:
+                break;
+        }
+
+        if (!player.isCreative()) {
+            player.getHeldItem(hand).shrink(1);
+        }
+
+        Vec3d rocketPos = new Vec3d(pos).addVector(0.5, 0.25, 0.5);
+        EntityFalcon9Base rocket = new EntityFalcon9Base(world, () -> rocketPos);
+        rockets.put(pos, rocket);
+        world.spawnEntity(rocket);
+
+        return true;
+    }
+
+    @Override
+    public boolean canPlaceBlockAt(World world, BlockPos pos) {
+        return super.canPlaceBlockAt(world, pos) && world.isBlockFullCube(pos.add(0, -1, 0));
+    }
+
+    @Override
+    public void breakBlock(World world, BlockPos pos, IBlockState state) {
+        getCenterLaunchpad(pos).ifPresent(centerPos -> rockets.remove(centerPos).setDead());
     }
 }
