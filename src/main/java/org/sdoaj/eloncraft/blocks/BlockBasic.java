82efa7c6d66e1ddff02046b2062272a98182d2b1
AdityaGupta1 <adityag@gmail.com>
2019-07-14 02:11:47 -0700

    close #30

--- src/main/java/org/sdoaj/eloncraft/blocks/BlockBasic.java
+++ src/main/java/org/sdoaj/eloncraft/blocks/BlockBasic.java
@@ -5,6 +5,8 @@ import net.minecraft.block.material.Material;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -16,6 +18,8 @@ import java.util.List;
 
 public class BlockBasic extends Block {
     private int burnTime = -1;
+
+    private boolean isBeaconBase = false;
 
     public BlockBasic(String name, Material material) {
         super(material);
@@ -68,6 +72,17 @@ public class BlockBasic extends Block {
         BlockBasic block = new BlockBasic(material + "_block", Material.IRON);
         block.setHardness(hardness).setResistance(resistance).setHarvestLevel("pickaxe", harvestLevel);
         block.setOreDictName("block" + StringUtil.capitalizeFirstLetter(material));
+        block.setBeaconBase();
         return block;
+    }
+
+    public void setBeaconBase() {
+        this.isBeaconBase = true;
+        addLore("Can be used as a beacon base");
+    }
+
+    @Override
+    public boolean isBeaconBase(IBlockAccess world, BlockPos pos, BlockPos beacon) {
+        return this.isBeaconBase;
     }
 }
