224ce343e64d0fee6af81800b87d6a029b1af9b2
AdityaGupta1 <adityag@gmail.com>
2019-07-16 01:30:11 -0700

    added info to falcon 9

--- src/main/java/org/sdoaj/eloncraft/jei/JEIPlugin.java
+++ src/main/java/org/sdoaj/eloncraft/jei/JEIPlugin.java
@@ -21,6 +21,7 @@ import org.sdoaj.eloncraft.blocks.machines.refinery.RefineryRecipes;
 import org.sdoaj.eloncraft.blocks.machines.workbench.WorkbenchRecipe;
 import org.sdoaj.eloncraft.blocks.machines.workbench.WorkbenchRecipes;
 import org.sdoaj.eloncraft.fluids.ModFluids;
+import org.sdoaj.eloncraft.items.ModItems;
 import org.sdoaj.eloncraft.jei.alloyfurnace.AlloyFurnaceRecipeCategory;
 import org.sdoaj.eloncraft.jei.alloyfurnace.AlloyFurnaceRecipeWrapper;
 import org.sdoaj.eloncraft.jei.crusher.CrusherRecipeCategory;
@@ -79,12 +80,19 @@ public class JEIPlugin implements IModPlugin {
         registry.addRecipeCatalyst(new ItemStack(ModBlocks.REFINERY), RefineryRecipeCategory.uid);
         registry.addRecipeCatalyst(new ItemStack(ModBlocks.LOX_COLLECTOR), LoxCollectorRecipeCategory.uid);
 
+
+        registry.addIngredientInfo(Collections.singletonList(new ItemStack(ModItems.FALCON9)), VanillaTypes.ITEM,
+                "To launch the Falcon 9,",
+                "- place it on a valid launchpad configuration",
+                "- load it up with fuel and LOX",
+                "- press \"LAUNCH\" from the launch controller",
+                "- and follow the prompts to begin your journey to space.");
         List<ItemStack> launchBlocks = new ArrayList<>();
         launchBlocks.add(new ItemStack(ModBlocks.LAUNCHPAD));
         launchBlocks.add(new ItemStack(ModBlocks.LAUNCH_CONTROLLER));
         registry.addIngredientInfo(launchBlocks, VanillaTypes.ITEM,
                 "A full launchpad requires a 7x7 square of launchpad blocks with a launch controller in the center of any of the edges.",
-                "", "Launchpads must be placed on solid blocks.");
+                "", "Launchpads can be placed only on blocks that are full cubes.");
         List<FluidStack> fuelFluids = new ArrayList<>();
         fuelFluids.add(new FluidStack(ModFluids.OIL, Fluid.BUCKET_VOLUME));
         fuelFluids.add(new FluidStack(ModFluids.RP1, Fluid.BUCKET_VOLUME));
