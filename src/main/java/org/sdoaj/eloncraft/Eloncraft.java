e6fd442e20b458e556b7656da0fb5bc1cf768658
AdityaGupta1 <adityag@gmail.com>
2019-07-18 18:45:35 -0700

    set version to 1.0.0

--- src/main/java/org/sdoaj/eloncraft/Eloncraft.java
+++ src/main/java/org/sdoaj/eloncraft/Eloncraft.java
@@ -38,7 +38,7 @@ public class Eloncraft {
 
     public static final String MODID = "eloncraft";
     public static final String NAME = "Eloncraft";
-    public static final String VERSION = "1.0-pre";
+    public static final String VERSION = "1.0.0";
 
     static {
         FluidRegistry.enableUniversalBucket();
